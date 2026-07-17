7547bd24280e9e4cb72c40186bcca3be13ebd36b
PenguinHat301 <DBiedermann15@gmail.com>
2026-07-17 14:55:43 -0400

    Working on card slot functionality

--- Scripts/card_base.gd
+++ Scripts/card_base.gd
@@ -1,8 +1,16 @@
 extends Control
 
+# Information about the card type and instantiation
 @export var card_data: CardData 
 @export var ID: int
 @export var cardID: int
+
+# var current location?
+var originalPosition: Vector2 
+var isHovered: bool
+var isDragging: bool
+var curDragOffset: Vector2
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -14,3 +22,32 @@ func _ready() -> void:
 	$Description.text = card_data.description
 	$HealthLabel.text = card_data.healthStat
 	$AttackLabel.text = card_data.attackStat
+
+
+func _process(delta: float) -> void:
+	if self.isDragging:
+		self.global_position = get_global_mouse_position() - curDragOffset
+
+
+func _gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			if event.pressed:
+				self.isDragging = true
+				self.z_index = 100
+				self.curDragOffset = get_global_mouse_position() - global_position
+				print("card clicked!")
+			elif self.isDragging:
+				self.isDragging = false
+				self.z_index = 0
+				self.curDragOffset = Vector2.ZERO
+				print("stopped hold")
+
+func _on_Card_mouse_entered() -> void:
+	self.z_index = 100
+	self.scale = Vector2(1.0,1.0)
+
+
+func _on_Card_mouse_exited() -> void:
+	self.z_index = 0
+	self.scale = Vector2(0.8,0.8)
---
+++ Scripts/card_slot.gd
@@ -0,0 +1,13 @@
+extends Control
+
+var occupied: bool
+var SpotID: int #0 to 4 player 1, 5 to 9 for player 2
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
